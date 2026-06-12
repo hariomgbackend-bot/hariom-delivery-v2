c79ecab07c364789dc677668f26747f56ff3c4bc
Your Name <your@email.com>
2026-06-12 19:41:47 +0530

    Added tally_debug panel 1

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ import cron from "node-cron";
 dotenv.config();
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase-service-account.json");
-//const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+//const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
